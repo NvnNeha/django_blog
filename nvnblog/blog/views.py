8372679f443c1697056364e261038d82ba02e420
Naveen Kumar <naveensaini2892@gmail.com>
2025-06-02 18:22:03 +0530

    configure settings.py,add templates and static files

--- nvnblog/blog/views.py
+++ nvnblog/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def starting_page(request):
+    return render(request, "blog/starting_page.html")
+
+def detailed_post(request):
+    return render(request, "blog/detailed_post.html")
+
+
+def all_posts(request):
+    return render(request,"blog/all_posts.html")
