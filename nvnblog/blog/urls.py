3d2b16c5624c8b9f174db30bebf0e5bb0bda0805
Naveen Kumar <naveensaini2892@gmail.com>
2025-06-02 18:22:03 +0530

    configure settings.py,add templates and static files

---
+++ nvnblog/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns= [
+    path("", views.starting_page, name="starting-page"),
+    path("/post", views.all_posts, name="all-posts"),
+    path("/post/<slug:slug>", views.detailed_post, name="detailed-post"),
+
+]
