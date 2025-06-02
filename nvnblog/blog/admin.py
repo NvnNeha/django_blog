75e7587597884bd7bf3a0416ed47991b5dc0fdbc
Naveen Kumar <naveensaini2892@gmail.com>
2025-06-02 19:13:12 +0530

    add login and register functionality

--- nvnblog/blog/admin.py
+++ nvnblog/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post, Tag, Comment
 
 # Register your models here.
+
+class PostAdminForm(admin.ModelAdmin):
+    prepopulated_fields = {"slug":("title",)}
+    list_display=("title","slug",)
+
+
+admin.site.register(Post, PostAdminForm)
+admin.site.register(Tag)
+admin.site.register(Comment)
--- nvnblog/blog/urls.py
+++ nvnblog/blog/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
-urlpatterns= [
+
+urlpatterns = [
     path("", views.starting_page, name="starting-page"),
-    path("post", views.all_posts, name="all-posts"),
-    path("post/<slug:slug>", views.detailed_post, name="detailed-post"),
+    path("posts/", views.all_posts, name="all-posts"),
+    path("posts/<slug:slug>", views.detailed_post, name="detailed-post"),
+    path("stored-posts/", views.stored_posts, name="stored-posts"),
+    path("create-post/", views.create_post, name="create-post"),
+    path("edit-post/<slug:slug>/", views.edit_post, name="edit-post"),
+    path("delete-post/<slug:slug>/", views.delete_post, name="delete-post"),
+    path("register/", views.register_user, name="register"),
+    path("login/", views.login_user, name="login")
 
 ]
